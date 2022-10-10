a947e0a84d84bc1955292584928b8eff8c411a68
Chung Nguyen <nvchung.zmg@gmail.com>
2022-10-10 14:25:41 +0700

    update

--- src/App.js
+++ src/App.js
@@ -14,60 +14,10 @@ import CheckAll from "./Components/Ref/CheckAll";
 import Color from "./Components/HOC/Color";
 import Counter from "./Components/HOC/Counter";
 function App() {
-
-    // const member = {
-    //     info: {
-    //         name: 'Hoàng An',
-    //         email: 'hoangan.web@gmail.com',
-    //         phone: '0388875179'
-    //     },
-
-    //     images: [
-    //         {
-    //             link: 'https://picsum.photos/500/300',
-    //             height: 200,
-    //             alt: 'Ảnh 1'
-    //         },
-
-    //         {
-    //             link: 'https://picsum.photos/500/300',
-    //             width: 350,
-    //             height: 250
-    //         },
-
-    //         {
-    //             link: 'https://picsum.photos/500/300',
-    //             width: 400,
-    //             height: 350
-    //         }
-    //     ],
-
-    //     posts: [
-    //         {
-    //             title: 'Bài viết 1',
-    //             content: 'Nội dung 1'
-    //         },
-
-    //         {
-    //             title: 'Bài viết 2',
-    //             content: 'Nội dung 2'
-    //         },
-
-    //         {
-    //             title: 'Bài viết 3',
-    //             content: 'Nội dung 3'
-    //         },
-
-    //         {
-    //             title: 'Bài viết 4',
-    //             content: 'Nội dung 4'
-    //         }
-    //     ]
-    // }
     
     return (
         <>
-            <Customer />
+            <Counter />
         </>
     );
 }
